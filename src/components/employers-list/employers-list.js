d475f89bd9b77e0b0f161936a841e0e90e3f82ff
Ilya <rubinshtein.ilya@gmail.com>
2022-12-23 23:21:10 +0600

    add key

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -4,9 +4,10 @@ import './employers-list.css';
 const EmployersList = ({data}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;
         return (
             // <EmployersListItem name={item.name} salary={item.salary}/>
-            <EmployersListItem {...item}/>
+            <EmployersListItem key={id} {...itemProps}/>
         )
     });
 
